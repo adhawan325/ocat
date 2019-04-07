331ff16885e6c625c8ac687e572d438884595e82
Ankur Dhawan <adhawan@SHIPLETS.AWSDIRECTORY.COM>
2019-04-07 15:49:44 -0700

    adding stuff

---
+++ modules/web/src/com/non/ocat/web/ChartsDemo.java
@@ -0,0 +1,10 @@
+package com.non.ocat.web;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("ocat_ChartsDemo")
+@UiDescriptor("charts-demo.xml")
+public class ChartsDemo extends Screen {
+}
